8dcb6cf22a17b890eadc044f38134d0acad70c90
cynbee <cynbee.1999@gmail.com>
2019-03-17 01:30:16 +0700

    Edit

---
+++ HTMLall/web/startgame/check.js
@@ -0,0 +1,5 @@
+function chkString() {
+    if (document.frmMain.txtString.value.length < 5 || document.frmMain.txtString.value.length > 15) {
+        alert('Please input name (5-15 Character) .');
+        return false;
+    }
