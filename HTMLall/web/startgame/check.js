80c6563cd940be01597e78a2a843fdef7a8651ae
Natapan <61070046@kmitl.ac.th>
2019-03-17 01:34:32 +0700

    up

--- HTMLall/web/startgame/check.js
+++
@@ -1,5 +0,0 @@
-function chkString() {
-    if (document.frmMain.txtString.value.length < 5 || document.frmMain.txtString.value.length > 15) {
-        alert('Please input name (5-15 Character) .');
-        return false;
-    }
